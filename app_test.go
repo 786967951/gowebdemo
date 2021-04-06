c45fe352c956f0fcaa69dc4d2116087cab615f35
786967951 <786967951@qq.com>
2021-04-06 14:58:30 +0800

    test

--- app_test.go
+++ app_test.go
@@ -27,11 +27,3 @@ func Test_GetNameLen_3(t *testing.T) {
 		t.Log("test passed.")
 	}
 }
-
-func Test_GetNameLen_4(t *testing.T) {
-	if l := getNameLen("andrew"); l != 5 {
-		t.Error("test failed, the length of andrew string is not correct.")
-	} else {
-		t.Log("test passed.")
-	}
-}
